36b418cae34e7e76dd27db6cfa699e18db0d1b9c
GarrettSwit <109528396+GarrettSwit@users.noreply.github.com>
2022-08-15 12:11:32 -0700

    Initial commit

---
+++ Source/CGSProject/CGSProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CGSProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CGSProject, "CGSProject" );
