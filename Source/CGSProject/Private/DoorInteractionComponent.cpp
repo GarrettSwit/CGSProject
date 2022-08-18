ec5fd0f7f8bb0177bf6df9e912ec74161a1e9abf
GarrettSwit <109528396+GarrettSwit@users.noreply.github.com>
2022-08-18 08:30:20 -0700

    Got opposite doors working
    Getting keys working now

--- Source/CGSProject/Private/DoorInteractionComponent.cpp
+++ Source/CGSProject/Private/DoorInteractionComponent.cpp
@@ -5,6 +5,7 @@
 #include "Engine/TriggerBox.h"
 #include "Engine/World.h"
 
+
 // Sets default values for this component's properties
 UDoorInteractionComponent::UDoorInteractionComponent()
 {
@@ -21,7 +22,7 @@ void UDoorInteractionComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	StartRotation = GetOwner()->GetActorRotation();
-	FinalRotation = GetOwner()->GetActorRotation() + DesiredRotation;
+	FinalRotation = GetOwner()->GetActorRotation();
 	
 	CurrentRotationTime = 0.0f;
 	// ...
@@ -29,14 +30,48 @@ void UDoorInteractionComponent::BeginPlay()
 }
 
 
+
+
 // Called every frame
 void UDoorInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
-	if (CurrentRotationTime < TimeToRotate) {
-		APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-		if (PlayerPawn && TriggerBox->IsOverlappingActor(PlayerPawn)) {
+	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (PlayerPawn && TriggerBox->IsOverlappingActor(PlayerPawn)) {	
+		if (GEngine) {
+			GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, TEXT("overlapped!"));
+		}
+		if (CurrentRotationTime < TimeToRotate) {  //this tells it to start rotating
+			if (GEngine) {
+				GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, TEXT("Began Rotation!"));
+			}
+			while (!HasCaluclatedVector) {
+				const float DotProduct = FVector::DotProduct(PlayerPawn->GetActorForwardVector(), GetOwner()->GetActorForwardVector());
+				if (DotProduct > 0.0f) {
+					if (GEngine) {
+						GEngine->AddOnScreenDebugMessage(-1, 0.2f, FColor::Yellow, TEXT("Same Direction"));
+					}
+					//same direction
+					FinalRotation += FRotator(0.0f, 50.0f, 0.0f);
+					HasCaluclatedVector = true;
+				}
+				else if (DotProduct == 0.0f) {
+					if (GEngine) {
+						GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Perpindicular"));
+					}
+					//do nothing
+
+				}
+				else if (DotProduct < 0.0f) {
+					if (GEngine) {
+						GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("OppositeDirection"));
+					}
+					//opposite direction
+					FinalRotation += FRotator(0.0f, -50.0f, 0.0f);
+					HasCaluclatedVector = true;
+				}
+			}
+
 			CurrentRotationTime += DeltaTime;
 			const float TimeRatio = FMath::Clamp(CurrentRotationTime / TimeToRotate, 0.0f, 1.0f);
 			const float RotationAlpha = OpenCurve.GetRichCurveConst()->Eval(TimeRatio);
@@ -44,6 +79,7 @@ void UDoorInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 			GetOwner()->SetActorRotation(CurrentRotation);
 		}
 	}
+	
 	// ...
 }
 
--- Source/CGSProject/Public/DoorInteractionComponent.h
+++ Source/CGSProject/Public/DoorInteractionComponent.h
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+//#include "Math/Vector.h"
 #include "Math/UnrealMathUtility.h"
 #include "GameFramework/Actor.h"
 #include "CoreMinimal.h"
@@ -36,12 +37,17 @@ protected:
 
 	UPROPERTY(EditAnywhere)
 		ATriggerBox* TriggerBox;
+
 	UPROPERTY(EditAnywhere)
 	FRuntimeFloatCurve OpenCurve;
+
+	bool HasCaluclatedVector = false;
+	
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	 void BeginOverLap(UPrimitiveComponent* OverLappedComponent);
 
 		
 };
